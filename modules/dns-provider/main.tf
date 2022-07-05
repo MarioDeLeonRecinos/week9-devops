68d2c0ce65d771ebd31b1ddd0264678e147a124c
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-07-05 14:05:49 -0600

    test module

---
+++ modules/dns-provider/main.tf
@@ -0,0 +1,52 @@
+resource "google_dns_managed_zone" "default" {
+  name        = var.name
+  dns_name    = var.dns_name
+  description = "Module generated DNS zone ${var.name}"
+
+  visibility = "public"
+
+  labels = var.labels
+}
+
+resource "google_compute_global_address" "default" {
+  name         = "reserved_ip_default"
+  address_type = "EXTERNAL"
+}
+
+# reserved IP address
+resource "google_compute_global_address" "subdomains" {
+  for_each     = var.subdomain
+  name         = "reserved_ip_${each.value}"
+  address_type = "EXTERNAL"
+}
+
+resource "google_dns_record_set" "www-default" {
+  name       = "www.${google_dns_managed_zone.default.dns_name}"
+  type       = "CNAME"
+  ttl        = 300
+
+  managed_zone = google_dns_managed_zone.default.name
+
+  rrdatas = [google_dns_managed_zone.default.dns_name]
+}
+
+resource "google_dns_record_set" "default" {
+  name       = google_dns_managed_zone.default.dns_name
+  type       = "A"
+  ttl        = 300
+
+  managed_zone = google_dns_managed_zone.default.name
+
+  rrdatas = [google_compute_global_address.default.address]
+}
+
+resource "google_dns_record_set" "subdomains" {
+  for_each     = var.subdomain
+  name       = "${each.value}.${google_dns_managed_zone.default.dns_name}"
+  type       = "A"
+  ttl        = 300
+
+  managed_zone = google_dns_managed_zone.default.name
+
+  rrdatas = [google_compute_global_address.subdomains[each.key].address]
+}
---
+++ modules/dns-provider/variables.tf
@@ -0,0 +1,21 @@
+variable "name" {
+    description = "Name of the resource"
+    type = string
+}
+
+variable "dns_name" {
+  description = "Name of the dns used. Must be unique."
+  type        = string
+}
+
+variable "labels" {
+  description = "Labels to set."
+  type        = map(string)
+  default     = {}
+}
+
+variable "subdomain" {
+  description = "Subdomains to set."
+  type        = set(string)
+  default     = []
+}
