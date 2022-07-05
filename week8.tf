a599b15f632866b1352789517815f06f69ad2f48
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-07-05 09:25:19 -0600

    test

--- week8.tf
+++ week8.tf
@@ -1,7 +1,7 @@
-resource "google_project_service" "vm-api" {
-  service  = "compute.googleapis.com"
-}
-
 resource "google_project_service" "dns-api" {
   service  = "dns.googleapis.com"
+}
+
+resource "google_project_service" "kubernetes-api" {
+  service  = "container.googleapis.com"
 }
