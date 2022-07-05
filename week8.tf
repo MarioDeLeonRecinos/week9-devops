01d97e77b3d14acd8005a0ce2a13b80bd0278bac
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-07-05 10:04:39 -0600

    test

--- week8.tf
+++ week8.tf
@@ -2,9 +2,9 @@ resource "google_project_service" "dns_api" {
   service  = "dns.googleapis.com"
 }
 
-#resource "google_project_service" "kubernetes_api" {
-#  service  = "container.googleapis.com"
-#}
+resource "google_project_service" "kubernetes_api" {
+  service  = "container.googleapis.com"
+}
 
 resource "google_compute_network" "vpc_network" {
   name                    = "vpc-network"
@@ -46,5 +46,47 @@ resource "google_dns_record_set" "default" {
 
   managed_zone = google_dns_managed_zone.default.name
 
-  rrdatas = [google_compute_global_address.default.address] //google_compute_instance.frontend.network_interface[0].access_config[0].nat_ip
+  rrdatas = [google_compute_global_address.default.address]
+}
+
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account Kubernetes"
+}
+
+resource "google_container_cluster" "primary" {
+  name = "my-gke-cluster"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+network = google_compute_network.vpc_network.name
+  release_channel {
+    channel = "REGULAR"
+  }
+}
+
+data "google_container_engine_versions" "default" {
+  version_prefix = "1.24."
+}
+
+output "stable_channel_version" {
+  value = data.google_container_engine_versions.default.release_channel_default_version["STABLE"]
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
+  version    = data.google_container_engine_versions.default.release_channel_default_version["STABLE"]
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-small"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
