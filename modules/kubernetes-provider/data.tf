2bbb55c195f2b72cf381eccd0e76aee849521771
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-07-09 13:26:46 -0600

    fix

--- modules/kubernetes-provider/data.tf
+++ modules/kubernetes-provider/data.tf
@@ -2,13 +2,3 @@
 data "google_container_engine_versions" "default" {
   version_prefix = "1.24."
 }
-
-data "google_iam_policy" "admin" {
-  binding {
-    role = "roles/container.admin"
-
-    members = [
-      "allUsers"
-    ]
-  }
-}
